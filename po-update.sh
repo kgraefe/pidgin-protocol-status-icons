e8fed111cbdf6e0f1c5129bf538a972ccfccb750
Konrad Gräfe <kgraefe@ponyslaystation>
2010-06-01 16:19:29 +0200

    * updated build files * added POT template

--- po-update.sh
+++ po-update.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 cd po
-intltool-update de
+echo "Updating POT template..."
+intltool-update -po
+
+for f in *.po
+do echo "Checking ${f%.po} language file..."
+intltool-update ${f%.po}
+done
+
